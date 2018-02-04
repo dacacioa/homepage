14f199f3d62982d0e164026eabd22af9f76ed880
David Acacio Albareda <dacacioa@gmail.com>
2018-02-04 12:11:54 +0100

    Delete js folder

--- js/parser.js
+++
@@ -1,7 +0,0 @@
-function fetch(websiteUrl).then((res) => {
-  res.text().then((htmlTxt) => {
-    var domParser = new DOMParser()
-    let doc = domParser.parseFromString(htmlTxt, 'text/html')
-    var feedUrl = doc.querySelector('link[type="application/rss+xml"]').href
-  })
-}).catch(() => console.error('Error in fetching the website'))
